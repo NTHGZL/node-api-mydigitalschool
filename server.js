a2f110bab5153dd5fc26a5e2ea6fc5878bf014a6
Nathan Gonzalez <nathangonzalez@outlook.fr>
2020-11-12 16:25:09 +0100

    fin de journée

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const Logger = require('./tools/logger')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
 var router = express.Router();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(Logger);
 
 app.use(router);
 
 app.use('/', require('./routes'))
+app.use('/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
